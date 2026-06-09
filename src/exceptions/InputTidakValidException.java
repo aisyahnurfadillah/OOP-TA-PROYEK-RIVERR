8267182111d57003797c63c2bc0a44fcf9e9c678
Fadillaw <aisyahnurfadillah2006@gmail.com>
2026-06-09 14:36:48 +0700

    (4) tambah custom exception InputTidakValidException

---
+++ src/exceptions/InputTidakValidException.java
@@ -0,0 +1,19 @@
+﻿package exceptions;
+
+/**
+ * BAGIAN C - Custom Exception
+ * @author Aisyah Nur Fadillah (254311001) - Role 4: QA & Repo Master
+ */
+public class InputTidakValidException extends Exception {
+
+    private final String namaField;
+
+    public InputTidakValidException(String namaField, String pesan) {
+        super("[VALIDASI GAGAL] Field '" + namaField + "': " + pesan);
+        this.namaField = namaField;
+    }
+
+    public String getNamaField() {
+        return namaField;
+    }
+}
